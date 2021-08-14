92a5696174fcd68277dee64e6180bf0aff902879
Mohammed Shafeeq M <mshafeeqmsmc@gmail.com>
2021-08-14 15:18:10 +0000

    New feature upgraded

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function myfun() {
+function myfun1() {
 
 	document.getElementById("ok").innerHTML = "HI, I AM JAVASCRIPT";
+}
+
+
+function myfun2() {
+
+	document.getElementById("ok").innerHTML = "";
 }
